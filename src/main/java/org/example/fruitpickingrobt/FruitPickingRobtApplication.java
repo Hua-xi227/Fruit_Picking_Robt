db2d34ef3ff450116de2a2df2aacd90bd32bf985
Hua-xi227 <1500183776@qq.com>
2026-06-01 02:15:08 +0800

    Initial Spring Boot project

---
+++ src/main/java/org/example/fruitpickingrobt/FruitPickingRobtApplication.java
@@ -0,0 +1,13 @@
+package org.example.fruitpickingrobt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitPickingRobtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FruitPickingRobtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/fruitpickingrobt/FruitPickingRobtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fruitpickingrobt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitPickingRobtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
