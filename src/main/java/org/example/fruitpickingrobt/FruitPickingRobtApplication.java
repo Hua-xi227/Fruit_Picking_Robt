3a8a131eea24821dff5d3922e92d505164e2645f
Hua-xi227 <1500183776@qq.com>
2026-06-02 01:00:11 +0800

    基本框架

--- src/main/java/org/example/fruitpickingrobt/FruitPickingRobtApplication.java
+++ src/main/java/org/example/fruitpickingrobt/FruitPickingRobtApplication.java
@@ -9,17 +9,5 @@ public class FruitPickingRobtApplication {
     public static void main(String[] args) {
         SpringApplication.run(FruitPickingRobtApplication.class, args);
     }
-    class ABC{
-        public void a(){
-            System.out.println("a");
-        }
-        public void b(){
-            System.out.println("b");
-        }
-        public void c(){
-            System.out.println("c");
-        }
-    }
-
 
 }
